eda17c37e4d4fc3f00ff2986f8c51e232b8ec599
Sawako <saturnoxspace@outlook.com>
2020-03-29 10:53:14 -0600

    [PARTE 4]: Agregado rutas hijas al componente dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard-routes';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', component: DashboardComponent },
+  { path: '', component: DashboardComponent, children: dashboardRoutes },
   { path: '**', redirectTo: '' },
 ];
 
