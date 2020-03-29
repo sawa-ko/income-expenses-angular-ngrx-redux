76af13b3f724399dcf58a18e7b988a8310d27d6c
Sawako <saturnoxspace@outlook.com>
2020-03-29 11:07:36 -0600

    [PARTE 5]: Agregado formulario reactivo a la pagina de registro.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,11 +27,8 @@ import { SidebarComponent } from './share/sidebar/sidebar.component';
     NavbarComponent,
     SidebarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register.component.html',
-  styles: []
+  styles: [],
 })
 export class RegisterComponent implements OnInit {
+  public registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
 }
