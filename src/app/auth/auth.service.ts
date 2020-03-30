d6c1957f011aa39bac34e4d16f517c832f8b1f7f
Sawako <saturnoxspace@outlook.com>
2020-03-29 19:04:19 -0600

    [PARTE 11]: Se agrego funcion para guardar los datos del usuario en la base de datos de Firestore.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import 'firebase/firestore';
 
 import { User } from 'firebase';
 import { map } from 'rxjs/operators';
@@ -10,13 +12,27 @@ import Swal from 'sweetalert2';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private router: Router) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router,
+    private firestore: AngularFirestore,
+  ) {}
 
   public crearUsuario(nombre: string, email: string, password: string) {
     this.auth
       .createUserWithEmailAndPassword(email, password)
-      .then(() => {
-        this.router.navigate(['/dashboard']);
+      .then((resp: any) => {
+        this.firestore
+          .collection(resp.user.uid)
+          .doc('usuario')
+          .set({ nombre, email, password, uid: resp.user.uid })
+          .then(() => {
+            this.router.navigate(['/dashboard']);
+          })
+          .catch(error => {
+            this.auth.signOut();
+            Swal.fire('Tenemos problemas', error.message, 'error');
+          });
       })
       .catch(error => {
         Swal.fire('Tenemos problemas', error.message, 'error');
